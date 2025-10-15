992714add9f2df3e9fd3f392db41a1ae73f3e987
Edwin Kolotilov <73223160+edwinn27@users.noreply.github.com>
2025-10-15 12:16:59 +0200

    Update getQuote.js

--- getQuote.js
+++ getQuote.js
@@ -5,8 +5,8 @@ import dotenv from "dotenv";
 import os;
 dotenv.config();
 
-const TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-const TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 const BASE_WALLET = process.env.BASE_WALLET;
 const SOLANA_WALLET = process.env.SOLANA_WALLET;
@@ -182,4 +182,5 @@ async function mainLoop() {
 }
 
 mainLoop();
+
 
